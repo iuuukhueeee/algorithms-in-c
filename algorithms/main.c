7ecce3b2a4a06cb15b7919a88bae77ce62df02e6
Thien <thien.nguyen@saigonai.com>
2025-03-18 09:25:01 +0700

    feat: initial project

---
+++ algorithms/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  algorithms
+//
+//  Created by Thien on 18/03/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
